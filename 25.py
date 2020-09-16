c92876dc032381708d6658a909bdd01f31671f3b
Vetarium <aronbestaron@gmail.com>
2020-09-16 19:33:41 +0600

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,32 +1,14 @@
-seconds = int(input("insert amount of seconds "))
+totalSeconds = int(input("Please enter a number of seconds:"))
 
+days = int(totalSeconds / 60 / 60 / 24)
+totalSeconds -= days * 60 * 60 * 24
 
-minutes = seconds/60
-seconds = seconds % 60
+hours = int(totalSeconds / 60 / 60)
+totalSeconds -= hours * 60 * 60
 
-hours = minutes/60
-hours = seconds/3600
+minutes = int(totalSeconds / 60)
+totalSeconds -= minutes * 60
 
-minutes = minutes % 60
+seconds = totalSeconds
 
-days = hours/24
-days= minutes/1440
-days = seconds/86400
-
-hours = hours % 60
-hours = minutes % 60
-hours = seconds % 3600
-
-weeks = days/7
-weeks = hours/168
-weeks = minutes/10080
-weeks = seconds/604800
-
-days = days % 1
-days = hours % 24
-days = minutes % 1440
-days = seconds % 86400
-
-
-
-print (weeks, 'weeks', days, 'days', hours, 'hours', minutes, 'minutes', seconds, 'seconds')
+print("The duration is: ", days,"days", hours,"hours",  minutes, "minute", seconds, "seconds")
